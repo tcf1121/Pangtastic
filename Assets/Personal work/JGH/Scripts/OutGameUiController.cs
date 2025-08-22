175c300442850c09d09933b668ed8c0562c77fd3
PHSTD <c.oadib.autoan123@gmail.com>
2025-08-22 17:51:47 +0900

    [Fix] 시작했을때 환경 설정에 맞게 작동하도록

--- Assets/Personal work/JGH/Scripts/OutGameUiController.cs
+++ Assets/Personal work/JGH/Scripts/OutGameUiController.cs
@@ -5,7 +5,23 @@ public class OutGameUiController : MonoBehaviour
 {
     private void Start()
     {
+        // 배경음, 효과음 설정에 따라 초기 볼륨 조정 :: S
+        if (PlayerPrefs.GetInt("BackgroundMusicOnOff_Setting") == 0)
+        {
+            AudioController.Instance.BgmAudioSource.volume = 0f;
+        }
+        
+        if (PlayerPrefs.GetInt("SfxMusicOnOff_Setting") == 0)
+        {
+            AudioController.Instance.SfxAudioSource.volume = 0f;   
+        }
+        // 배경음, 효과음 설정에 따라 초기 볼륨 조정 :: E
+        
+        // 배경음 정지
         AudioController.Instance.StopBGM();
+        
+        
+        // 게임 클리어 후 계속하기 누르면 로비 화면으로 넘어오고 게임 시작 화면 활성화 :: S
         if (InGameUiController.needStartSetting)
         {
             GameObject[] roots = SceneManager.GetActiveScene().GetRootGameObjects();
@@ -18,6 +34,7 @@ public class OutGameUiController : MonoBehaviour
             }
             InGameUiController.needStartSetting = false;
         }
+        // 게임 클리어 후 계속하기 누르면 로비 화면으로 넘어오고 게임 시작 화면 활성화 :: E
     }
 
     public void ChangeInGameScene()
