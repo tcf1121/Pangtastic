cff429054584b8e4ecb3cbbd126ea30b172858e5
woojinjang193 <ryanjang193@gmail.com>
2025-08-27 23:32:16 +0900

    [feat] 게임 클리어 보상
    특수 블록 제공 (일단 로그로)
    점수 합산 (ScoreManager)

--- Assets/Personal work/JWJ/Scripts/CustomerFlowController.cs
+++ Assets/Personal work/JWJ/Scripts/CustomerFlowController.cs
@@ -1,3 +1,4 @@
+using KDJ;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,6 +10,8 @@ using UnityEngine.UI;
 public class CustomerFlowController : MonoBehaviour
 {
     [SerializeField] private CustomerOrderController _customerOrder;
+    //[SerializeField] private BoardManager _boardManager;
+    [SerializeField] private ScoreManager _scoreManager;
     [SerializeField] private Image _customerImage; // 손님 이미지
 
     private CustomerSO _curCustomer;
@@ -24,6 +27,14 @@ public class CustomerFlowController : MonoBehaviour
         if (_customerOrder == null)
         {
             _customerOrder = FindObjectOfType<CustomerOrderController>();
+        }
+        //if (_boardManager == null)
+        //{
+        //    _boardManager = FindObjectOfType<BoardManager>();
+        //}
+        if(_scoreManager == null)
+        {
+            _scoreManager = FindObjectOfType<ScoreManager>();
         }
         _customerOrder.OnCustomerSuccess += OnCustomerSuccess;
         _customerOrder.OnCustomerFail += OnCustomerFail;
@@ -46,6 +57,7 @@ public class CustomerFlowController : MonoBehaviour
     public void SpawnCustomer()
     {
         StageSO curStage = StageManager.Instance.CurrentStage;
+
         _curCustomer = curStage.Customer;
 
         _customerImage.sprite = _curCustomer.CustomerPic;
@@ -57,20 +69,46 @@ public class CustomerFlowController : MonoBehaviour
         Debug.Log($"현재 스테이지 {curStage.StageID}");
     }
 
-    private void OnCustomerSuccess(float percentage)
+    private void OnCustomerSuccess(CustomerSO customer, float percentage)
     {
-        StageClear(percentage);
+        StageClear(customer, percentage);
     }
 
     private void OnSpecialCustomerRewardGiven(float percentage)
     {
-        Debug.Log($"성공 인내심 {percentage}%. 보상제공");
-        //특수블록 확률은 깃[손님 유형] 참고
+        int percent = Mathf.FloorToInt(percentage);
+        Debug.Log($"스페셜 중간 성공 인내심 {percent}%. 보상제공");
+
+        for (int i = 0; i < 2; i++) //보상 개수 두개
+        {
+            int rand = UnityEngine.Random.Range(0, 100);
+            Debug.Log($"랜덤 숫자 {rand}");
+            if (rand >= 90)
+            {
+                Debug.Log("도넛상자 제공");
+            }
+            else if (rand >= 68)
+            {
+                Debug.Log("팝콘 제공");
+            }
+            else if (rand >= 46)
+            {
+                Debug.Log("우유 제공");
+            }
+            else if (rand >= 23)
+            {
+                Debug.Log("밀대 세로 제공");
+            }
+            else if (rand >= 0)
+            {
+                Debug.Log("밀대 가로 제공");
+            }
+        }
     }
 
     private void OnSpecialCustomerSuccess(CustomerSO customer, float averagePercent)
     {
-        StageClear(averagePercent);
+        StageClear(customer, averagePercent);
     }
 
     private void OnCustomerFail()
@@ -83,15 +121,36 @@ public class CustomerFlowController : MonoBehaviour
     //    StageFail();
     //}
 
-    private void StageClear(float percentage)
+    private void StageClear(CustomerSO customer, float percentage)
     {
-        Debug.Log($"스테이지 클리어. 보상 기준 인내심{percentage}");
+        int percent = Mathf.FloorToInt(percentage); //int 로 변경
+
+        Debug.Log($"스테이지 클리어. 보상 기준 인내심{percent}");
 
         StageManager.Instance.AdvanceStage();
 
-        OnStageCleared?.Invoke();
         //특수블록 보상 도넛상자 제외
+        if (percent >= 70)
+        {
+            //_boardManager.Spawner.SpawnRandomBlock
+            Debug.Log("블록 4개 제공");
+        }
+        else if (percent >= 40)
+        {
+            Debug.Log("블록 3개 제공");
+        }
+        else if(percent >= 17)
+        {
+            Debug.Log("블록 2개 제공");
+        }    
+        else if(percent >= 0)
+        {
+            Debug.Log("블록 1개 제공");
+        }
 
+        _scoreManager.AddScore(percent * 10); //점수 전송
+
+        OnStageCleared?.Invoke(); //스테이지 클리어 이벤트
         StartCoroutine(TmpChangeSceneRoutine()); //임시 씬 넘기기 코루틴
     }
     
