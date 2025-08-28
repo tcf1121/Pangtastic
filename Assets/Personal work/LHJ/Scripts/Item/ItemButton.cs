ab6872d67d2e8da7e793d8fb787dd7e3542051b2
neosmu <hojunglee00@naver.com>
2025-08-28 19:14:57 +0900

    [Fix] 아이템 기능 수정
    아이템이 고정된 위치에서만 발생되는 현상 수정

--- Assets/Personal work/LHJ/Scripts/Item/ItemButton.cs
+++ Assets/Personal work/LHJ/Scripts/Item/ItemButton.cs
@@ -8,45 +8,23 @@ namespace LHJ
     public class ItemButton : MonoBehaviour
     {
         [SerializeField] private ItemType _type;
-        [SerializeField] private ItemCheck _checker;
         [SerializeField] private Button _button;
-
-        private bool _selected = false;
 
         private void Awake()
         {
             if (_button == null) _button = GetComponent<Button>();
             _button.onClick.AddListener(OnClick);
-
-            if (_checker != null)
-                _checker.OnDeselected += ForceDeselect;
-        }
-
-        private void OnDestroy()
-        {
-            if (_checker != null)
-                _checker.OnDeselected -= ForceDeselect;
         }
 
         private void OnClick()
         {
-            if (_checker == null) return;
+            var manager = CopyBoardManager.Instance;
+            if (manager == null) return;
 
-            if (_selected)
-            {
-                _checker.Deselect();
-                _selected = false;
-            }
+            if (manager.IsItemSelected && manager.SelectedItemType == _type)
+                manager.ClearItemSelection();
             else
-            {
-                _checker.Select(_type);
-                _selected = true;
-            }
-        }
-
-        public void ForceDeselect()
-        {
-            _selected = false;
+                manager.SelectItem(_type);
         }
     }
 }
