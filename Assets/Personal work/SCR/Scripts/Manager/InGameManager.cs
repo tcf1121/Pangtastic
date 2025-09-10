92491def7b55dfffd81837568be008a1475e2d20
woojinjang193 <ryanjang193@gmail.com>
2025-09-10 16:56:56 +0900

    [feat] 슬라이더 연출 추가, 추가 인내심 로직 하나로 합침

--- Assets/Personal work/SCR/Scripts/Manager/InGameManager.cs
+++ Assets/Personal work/SCR/Scripts/Manager/InGameManager.cs
@@ -162,7 +162,7 @@ public class InGameManager : MonoBehaviour
         if (Manager.User.CanUseCoin(_useCoin))
         {
             Manager.User.UseCoin(_useCoin);
-            orderStateController.HalfPatience();
+            orderStateController.AddPatience(50f);
             continueUI.SetActive(false);
             BoardManager.SetTouch(true);
         }
