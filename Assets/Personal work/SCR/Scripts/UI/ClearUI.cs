a309072a75daa1ce61618607f24c6d0f3becf10e
FeAnd <tcf1268@gmail.com>
2025-09-17 13:12:43 +0900

    [Update]

--- Assets/Personal work/SCR/Scripts/UI/ClearUI.cs
+++ Assets/Personal work/SCR/Scripts/UI/ClearUI.cs
@@ -7,8 +7,8 @@ public class ClearUI : MonoBehaviour
 {
     [SerializeField] private RectTransform star;    // 움직일 별
     [SerializeField] private RectTransform targetStar;  // 목표 위치 (별 카운트 아이콘)
-    [SerializeField] private RectTransform coin;    // 움직일 별
-    [SerializeField] private RectTransform targetCoin;  // 목표 위치 (별 카운트 아이콘)
+    [SerializeField] private RectTransform coin;    // 움직일 코인
+    [SerializeField] private RectTransform targetCoin;  // 목표 위치 (코인 카운트 아이콘)
     [SerializeField] private Button button;
     private int completedCount = 0;
     private int totalToComplete = 2;
@@ -46,9 +46,6 @@ public class ClearUI : MonoBehaviour
         seq.OnComplete(() =>
         {
             onComplete?.Invoke();
-            completedCount++;
-            if (completedCount >= totalToComplete)
-                finMove?.Invoke();
         });
     }
 
@@ -57,7 +54,11 @@ public class ClearUI : MonoBehaviour
         MoveUIItem(star, target, () =>
         {
             Manager.User.AddStar(1);
+            Manager.Audio.PlaySFX("Star_Add");
             star.gameObject.SetActive(false);
+            completedCount++;
+            if (completedCount >= totalToComplete)
+                finMove?.Invoke();
         });
     }
 
@@ -66,7 +67,11 @@ public class ClearUI : MonoBehaviour
         MoveUIItem(coin, target, () =>
         {
             Manager.User.AddCoin(InGameManager.GetCoin());
+            Manager.Audio.PlaySFX("Coin_Add");
             coin.gameObject.SetActive(false);
+            completedCount++;
+            if (completedCount >= totalToComplete)
+                finMove?.Invoke();
         });
     }
 }
