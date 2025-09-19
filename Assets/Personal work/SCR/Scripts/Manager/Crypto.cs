cf97841b009d6880f59a9adee82d8288ab87b189
FeAnd <tcf1268@gmail.com>
2025-09-19 12:54:48 +0900

    [Fix] 암호화 키 변경

--- Assets/Personal work/SCR/Scripts/Manager/Crypto.cs
+++ Assets/Personal work/SCR/Scripts/Manager/Crypto.cs
@@ -1,10 +1,30 @@
+using System;
 using System.Security.Cryptography;
 using System.Text;
+using UnityEngine;
 
 public static class Crypto
 {
-    private static string key = "VkdxktmxlrskRh06"; // 반드시 16/24/32 글자
-    private static string iv = "VkdxktmxlrskRh06";  // 반드시 16 글자
+    private static string key = GenerateDeviceKey(32); // 반드시 16/24/32 글자
+    private static string iv = GenerateDeviceKey(16);  // 반드시 16 글자
+
+
+    public static string GenerateDeviceKey(int length)
+    {
+        // 기기 고유 ID를 가져와서 해시 함수를 적용
+        string deviceId = SystemInfo.deviceUniqueIdentifier;
+        byte[] deviceIdBytes = Encoding.UTF8.GetBytes(deviceId);
+
+        // SHA256 해시 함수를 사용하여 키 생성
+        SHA256 sha256 = SHA256.Create();
+        byte[] keyBytes = sha256.ComputeHash(deviceIdBytes);
+
+        byte[] finalBytes = new byte[length];
+        Array.Copy(keyBytes, finalBytes, length);
+
+        // 바이트 배열을 문자열로 변환하여 키로 사용
+        return BitConverter.ToString(finalBytes).Replace("-", "").ToLower();
+    }
 
     public static string Encrypt(string plainText)
     {
