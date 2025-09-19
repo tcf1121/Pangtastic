15906526401cae311e612e2c935d6c1e8d9d20a7
FeAnd <tcf1268@gmail.com>
2025-09-19 11:36:10 +0900

    [Update] 로그인 화면 로고 변경

--- Assets/Personal work/SCR/Scripts/Manager/TimerManager.cs
+++ Assets/Personal work/SCR/Scripts/Manager/TimerManager.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 
 public class TimerManager : Singleton<TimerManager>
 {
+    public Coroutine ADCor;
+    private Action _adFin;
+    private bool _isGame;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _isGame = false;
+        _adFin += EndADTime;
+    }
 
     public IEnumerator StartTimer(Coroutine cor, float CycleTime,
      Action<float> onCorTimer = null, Action onTimerFinished = null)
@@ -16,6 +26,46 @@ public class TimerManager : Singleton<TimerManager>
             {
                 _cycle -= Time.unscaledDeltaTime;
                 onCorTimer?.Invoke(_cycle);
+                yield return null;
+            }
+            else
+                break;
+        }
+        onTimerFinished?.Invoke();
+    }
+
+    public void StartGame()
+    {
+        _isGame = true;
+        if (ADCor == null) StartADTimer(180, _adFin);
+    }
+
+    public void EndGame()
+    {
+        _isGame = false;
+    }
+
+    private void EndADTime()
+    {
+        if (ADCor != null)
+        {
+            StopCoroutine(ADCor);
+            ADCor = null;
+        }
+    }
+
+    private IEnumerator StartADTimer(float CycleTime,
+     Action onTimerFinished = null)
+    {
+        float _cycle = CycleTime;
+        while (true)
+        {
+            if (_cycle > 0)
+            {
+                if (_isGame)
+                {
+                    _cycle -= Time.unscaledDeltaTime;
+                }
                 yield return null;
             }
             else
