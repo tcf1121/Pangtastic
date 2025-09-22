fe9d71f79b3a21a1d55f2ea367d664498f5c4de5
woojinjang193 <ryanjang193@gmail.com>
2025-09-22 20:20:56 +0900

    [feat] 캐릭터 회전, 애니메이션 적용

--- Assets/Personal work/JWJ/Scripts/Customizing/CosmeticCategory.cs
+++ Assets/Personal work/JWJ/Scripts/Customizing/CosmeticCategory.cs
@@ -5,7 +5,7 @@ public enum CosmeticCategory
     None = 0,   // 기본값 또는 미설정
     Skin = 1,   // 피부
     Face = 2,   // 얼굴
-    Hair = 3,   // 헤어
+    Headwear = 3,   // 헤어
     Eyewear = 4,// 안경
     Bag = 5,    // 가방
     Accessory = 6 // 액세서리
